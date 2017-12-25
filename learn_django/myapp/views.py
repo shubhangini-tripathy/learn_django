2ea0a740320670380aa523f19fd726eaf69f5c86
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2017-12-26 01:36:52 +0530

    Write hello world app.

--- learn_django/myapp/views.py
+++ learn_django/myapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
 
 def hello(request):
-   return render(request, "myapp/template/hello.html", {})
+    text = "<h1>welcome to my app!</h1>"
+    return HttpResponse(text)
