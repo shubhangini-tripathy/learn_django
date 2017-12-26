8e3eea27f3022178e8f2f3ba06e70c678a940110
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2017-12-26 18:58:41 +0530

    write view aas a template

--- learn_django/myapp/urls.py
+++ learn_django/myapp/urls.py
@@ -3,9 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.hello, name='hello'),
-    path('shubhi/',
-         views.shubhi, name='shubhi')
+    # path('hello/', views.hello, name='hello'),
+    # path('shubhi/', views.shubhi, name='shubhi'),
+    # path('ashu/', views.shubhi, name='ashu')
+    path('shubhi/', views.ShubhiPageView.as_view(), name='shubhi'),
+    path('ashu/', views.AshuPageView.as_view(), name='ashu'),
+
+
+
+
 
 
 ]
--- learn_django/myapp/views.py
+++ learn_django/myapp/views.py
@@ -1,11 +1,24 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
-def hello(request):
-    text = "<h1>welcome to my app!</h1>"
-    return HttpResponse(text)
+# def hello(request):
+#     text = "<h1>welcome to my app!</h1>"
+#     return HttpResponse(text)
 
 
-def shubhi(request):
-    text = "<h1>hello shubhi</h1>"
-    return HttpResponse(text)
+# def shubhi(request):
+#     text = "<h1>hello shubhi</h1>"
+#     return HttpResponse(text)
+
+# def ashu(request):
+#     text = "<h1>hello shubhi</h1>"
+#     return HttpResponse(text)
+
+from django.views.generic import TemplateView
+
+
+class ShubhiPageView(TemplateView):
+    template_name = 'shubhi.html'
+class AshuPageView(TemplateView):
+    template_name = 'ashu.html'
+
