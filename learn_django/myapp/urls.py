7ca1f7491e22f5b971dd2b9b2362b4e780ee6df3
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2017-12-26 18:07:55 +0530

    create a new view

--- learn_django/myapp/urls.py
+++ learn_django/myapp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.hello, name='hello')
+    path('hello/', views.hello, name='hello'),
+    path('shubhi/',
+         views.shubhi, name='shubhi')
+
+
 ]
--- learn_django/myapp/views.py
+++ learn_django/myapp/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 def hello(request):
     text = "<h1>welcome to my app!</h1>"
     return HttpResponse(text)
+
+
+def shubhi(request):
+    text = "<h1>hello shubhi</h1>"
+    return HttpResponse(text)
