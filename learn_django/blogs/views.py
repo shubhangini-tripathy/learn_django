d6cecbd599041b01efa9a289d5c00b44823dd5da
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2017-12-28 21:20:00 +0530

    push

--- learn_django/blogs/views.py
+++ learn_django/blogs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView ,DetailView
 
 from . models import Post
 
@@ -9,4 +9,11 @@ from . models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'blog.html'
+
+
+...
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'anything_you_want'
 
