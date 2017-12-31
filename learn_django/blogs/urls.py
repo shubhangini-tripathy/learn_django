986efe88903ba0ea0511de60089b0b80f52fbcf9
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2017-12-31 19:42:54 +0530

    make edit blog

--- learn_django/blogs/urls.py
+++ learn_django/blogs/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='home'),
     path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
-
-
+    path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/',
+        views.BlogUpdateView.as_view(), name='post_edit'),  
 ]
+
 
--- learn_django/blogs/views.py
+++ learn_django/blogs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView ,DetailView
+from django.views.generic.edit import CreateView ,UpdateView
 
 from . models import Post
 
@@ -11,9 +12,22 @@ class BlogListView(ListView):
     template_name = 'blog.html'
 
 
-...
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
     context_object_name = 'anything_you_want'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'text']
+    template_name = 'post_edit.html'
+
+
+
 
